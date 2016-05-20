6e3a2d8fb1fe2f9c479e87e26acfc18b62ad6b83
hashmat00 <hashmat00@aol.com>
2016-05-20 22:21:54 +0000

    create edit, update, show controlls with view templates with all error mesage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
       @article = Article.new(article_params)
         if @article.save
@@ -18,6 +22,16 @@ class ArticlesController < ApplicationController
        @article = Article.find(params[:id]) 
     end
     
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Your Article successfully been udpated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
     
     
 private
