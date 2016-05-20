bbc8ec82b94e325ac1433f66282f5a750e77f5d6
hashmat00 <hashmat00@aol.com>
2016-05-20 23:45:59 +0000

    extract redundency and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-    
+    before_action :set_article, only: [:edit, :show, :update, :destroy]
     
     def index
         @articles = Article.all    
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id]) // instead we used before action :set_article on top
     end
     
     def create
@@ -25,12 +25,12 @@ class ArticlesController < ApplicationController
     end
     
     def show
-       @article = Article.find(params[:id]) 
+    #   @article = Article.find(params[:id])  // instead we used before action :set_article on top
     end
     
     
     def update
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])  // instead we used before action :set_article on top
         if @article.update(article_params)
             flash[:notice] = "Your Article successfully been udpated"
             redirect_to article_path(@article)
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
     
     
     def destroy
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id]) // instead we used before action :set_article on top
         @article.destroy
         flash[:notice] = "Article was successfully deleted"
         redirect_to articles_path
@@ -51,6 +51,11 @@ class ArticlesController < ApplicationController
     
     
 private
+
+    def set_article
+        @article = Article.find(params[:id])    
+    end
+    
     def article_params
        params.require(:article).permit(:title, :description) 
     end
