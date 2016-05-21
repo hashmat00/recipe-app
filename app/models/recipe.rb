d644eec303733c925e8db886f81810a23d415f2b
hashmat00 <hashmat00@aol.com>
2016-05-21 22:56:29 +0000

    create chef and recipe associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ActiveRecord::Base
-    
-    
+    belongs_to :chef
+    validates :chef_id, presence: ture
+    validates :name, presence: true, length: { minimum: 3, maximum: 50}
+   validates :summary, presence: true, length: {minimum: 10, maximum: 150}
+   validates :description, presence: true, length: {minimum: 20, maximum: 500}   
     
 end
