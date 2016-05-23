ec5dacf6e51c029ab91847e963e119b086649a7d
hashmat00 <hashmat00@aol.com>
2016-05-23 04:42:00 +0000

    added create page, form, errors validations,

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,39 @@ class RecipesController < ApplicationController
     
     
     
+    
+    def new
+        @recipe = Recipe.new
+    end
+    
+    def create
+        @recipe = Recipe.new(recipe_params)
+       @recipe.chef = Chef.find(1)
+       if @recipe.save
+        flash[:success] = "You have successfully created the Recipe"
+        redirect_to recipes_path
+        else
+            render 'new'
+        end
+      end
+    
+    
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
+    
+    
+    
+    def update  
+    end
+    
+    
+    
+    private
+    
+        def recipe_params
+        params.require(:recipe).permit(:name, :summary, :description)
+    end
+    
+    
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,6 @@ class Recipe < ActiveRecord::Base
     validates :chef_id, presence: true
     validates :name, presence: true, length: { minimum: 3, maximum: 50}
    validates :summary, presence: true, length: {minimum: 10, maximum: 150}
-   validates :description, presence: true, length: {minimum: 20, maximum: 500}   
+   validates :description, presence: true, length: {minimum: 20, maximum: 1000}   
     
 end
