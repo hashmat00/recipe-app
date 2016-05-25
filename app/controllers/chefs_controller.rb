59454023c632c0d2b3ca4a4012b0dfd59e21fef1
hashmat00 <hashmat00@aol.com>
2016-05-25 05:46:19 +0000

    added new navbar, add chefs index, shows, edit page and also changed chefs index listing style

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,6 +1,8 @@
 class ChefsController < ApplicationController
 	
-
+    def index
+     @chefs = Chef.all
+    end
 
 	def new
 		@chef = Chef.new
@@ -18,7 +20,24 @@ class ChefsController < ApplicationController
 	end
 	
 	def edit
+	    @chef = Chef.find(params[:id])
 	    
+	end
+	
+	
+	def update
+	   @chef = Chef.find(params[:id])
+	   if @chef.update(chef_params)
+	       flash[:success] = "Your account has been updated sucessfully"
+	       redirect_to recipes_path
+	   else
+	       render 'edit'
+	   end
+	end
+	
+	def show
+	    @chef = Chef.find(params[:id])
+	    @recipes = @chef.recipes.paginate(page: params[:page], per_page: 4)
 	end
 	 
 
