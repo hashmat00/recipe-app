1f9fb5e52f01d918ec6a10ad9ccb7d6a50de3dd0
hashmat00 <hashmat00@aol.com>
2016-05-31 20:42:58 +0000

    removed comment form comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,6 @@
 class CommentsController < ApplicationController
     
-    # def new
-    #     @comment = Comment.new
-    # end
+   
     
     def create
         @comment = Comment.create(comment_params)
