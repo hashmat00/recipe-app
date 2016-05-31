7560d729914efe102e7c6ccbae8707c76d5498a5
hashmat00 <hashmat00@aol.com>
2016-05-31 00:44:27 +0000

    added region to picture uplaoder carrier wave

--- config/carrier_wave.rb
+++ config/carrier_wave.rb
@@ -13,16 +13,15 @@
 
 if Rails.env.production?
 
-    CarrierWave.configure do |config|
-    
-        config.fog_credentials = {
-        
-        :provider => 'AWS',
-        
-        :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-        
-        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-        :region                 => 'us-east-1'
+CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
+    :s3_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
+    :region                 => 'eu-west-1' 
         
         }
     
